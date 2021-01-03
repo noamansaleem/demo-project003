c3e29964d0606befb4a0cc736b38be6459730097
noamansaleem <noamantech@gmail.com>
2021-01-03 15:05:20 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Noaman Edit <code>src/App.js</code> and save to reload.
+          Noaman Saleem Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
